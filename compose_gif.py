5f61393fcd31d6caf1fbf26ba54ca7fef77cbd98
Ximu Zhang <ximuzh@gmail.com>
2021-04-24 14:44:30 -0400

    add fps as a parameter

--- compose_gif.py
+++ compose_gif.py
@@ -5,20 +5,18 @@ Created Date: 04/23/2021
 """
 
 import imageio
-import os
 import glob
-import numpy as np
 
 
-def compose_gif(img_path):
+def compose_gif(img_path, fps=10):
 	file_paths = glob.glob(pathname='%s/*.jpg' % img_path)
 	file_num = len(file_paths)
 
 	gif_images = []
 	for i in range(file_num):
 		gif_images.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
-	imageio.mimsave('./gif/a_star.gif', gif_images, fps=10)
+	imageio.mimsave('./gif/a_star.gif', gif_images, fps=fps)
 
 
 if __name__ == '__main__':
-	compose_gif('./gif/a_star')
+	compose_gif(img_path='./gif/a_star', fps=50)
