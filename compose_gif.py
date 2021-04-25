54cc72d08bd139937b851b5138d1e4fba6834e1b
Ximu Zhang <ximuzh@gmail.com>
2021-04-25 13:36:03 -0400

    Update compose_gif.py

--- compose_gif.py
+++ compose_gif.py
@@ -8,21 +8,22 @@ import imageio
 import glob
 
 
-def compose_gif(img_path, fps=10):
+def compose_gif(img_path, duration=1.0):
+	gif_name = img_path.split('/')[-1]
 	file_paths = glob.glob(pathname='%s/*.jpg' % img_path)
 	file_num = len(file_paths)
 
-	gif_images = []
+	frames = []
 	for i in range(file_num):
-		gif_images.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
-	imageio.mimsave('./gif/a_star.gif', gif_images, fps=fps)
+		frames.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
+	imageio.mimsave('./gif/%s.gif' % gif_name, frames, 'GIF', duration=duration)
 
 
 if __name__ == '__main__':
-	make_a_star_gif = False
+	make_a_star_gif = True
 	make_rrt_gif = True
 
 	if make_a_star_gif:
-		compose_gif(img_path='./gif/a_star', fps=50)
+		compose_gif(img_path='./gif/a_star', duration=0.1)
 	if make_rrt_gif:
-		compose_gif(img_path='./gif/rrt', fps=50)
+		compose_gif(img_path='./gif/rrt', duration=0.1)
