9ee12e970e6c1c30b33913ede4fdcb0d15c0bd68
Ximu Zhang <ximuzh@gmail.com>
2021-04-25 13:57:50 -0400

    Update compose_gif.py

--- compose_gif.py
+++ compose_gif.py
@@ -8,15 +8,19 @@ import imageio
 import glob
 
 
-def compose_gif(img_path, duration=1.0):
+def compose_gif(img_path, duration=0.1):
 	gif_name = img_path.split('/')[-1]
 	file_paths = glob.glob(pathname='%s/*.jpg' % img_path)
 	file_num = len(file_paths)
-
 	frames = []
 	for i in range(file_num):
-		frames.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
-	imageio.mimsave('./gif/%s.gif' % gif_name, frames, 'GIF', duration=duration)
+		if i % 10 == 0:  # read for every 10 frames
+			frames.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
+		if i == file_num - 1:  # read the last frame
+			frames.append(imageio.imread('%s/%d.jpg' % (img_path, i)))
+	duration_list = [duration] * len(frames)
+	duration_list[-1] = 3.0
+	imageio.mimsave('./gif/%s.gif' % gif_name, frames, 'GIF', duration=duration_list)
 
 
 if __name__ == '__main__':
@@ -24,6 +28,6 @@ if __name__ == '__main__':
 	make_rrt_gif = True
 
 	if make_a_star_gif:
-		compose_gif(img_path='./gif/a_star', duration=0.1)
+		compose_gif(img_path='./gif/a_star')
 	if make_rrt_gif:
-		compose_gif(img_path='./gif/rrt', duration=0.1)
+		compose_gif(img_path='./gif/rrt')
